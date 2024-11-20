694502285d560139cf55683773b8add57f248848
DESKTOP-K7E3CAS\chu03 <chu0326@naver.com>
2024-11-21 04:32:08 +0900

    21일 1차 공부

--- Endure/Endure/Main.cpp
+++ Endure/Endure/Main.cpp
@@ -1,10 +1,16 @@
 #include <iostream>
-
 using namespace std;
 
+class MyClass {
+public:
+    int count = 0;
+    // 디폴트 생성자
+    MyClass() {
+        cout << "Default constructor called" << endl;
+    }
+};
+
 int main() {
-
-	cout << "test" << endl;
-
-	return 0;
+    //MyClass obj = 10;  // 암시적 변환: int -> MyClass
+    return 0;
 }
