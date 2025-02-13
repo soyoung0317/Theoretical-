836a6671f3f685cf8f6ccb52894e92791f1fd49b
DESKTOP-K7E3CAS\chu03 <chu0326@naver.com>
2025-02-14 06:53:22 +0900

    12회차 스터디 준비

--- Endure/Endure/Main.cpp
+++ Endure/Endure/Main.cpp
@@ -1,33 +1,33 @@
 ﻿#include <iostream>
 #include <vector>
-#include <algorithm>
-#include <stdio.h>
-#include <stdlib.h>
 
 using namespace std;
 
-typedef struct TEST
-{
-    string str;  
-    int num;          
-}TEST;
+class Test {
+public:
+    Test() { std::cout << "Constructor\n"; }
+    Test(const Test&) { std::cout << "Copy Constructor\n"; }
+    Test(Test&&) noexcept { std::cout << "Move Constructor\n"; }
+};
+
+// 복사 (lvalue) 오버로딩
+void push_to_vector(std::vector<Test>& vec, const Test& obj) {
+    std::cout << "[push_to_vector] Copy Version\n";
+    vec.push_back(obj);
+}
+
+// 이동 (rvalue) 오버로딩
+void push_to_vector(std::vector<Test>& vec, Test&& obj) {
+    std::cout << "[push_to_vector] Move Version\n";
+    vec.push_back(obj); 
+}
+
 int main() {
-    int* ptr1 = (int*)malloc(sizeof(int));
-    *ptr1 = 10;
+    std::vector<Test> vec;
 
-    printf("ptr1 값[%d]\n", *ptr1);
-    int* ptr2 = (int*)realloc(ptr1, sizeof(int)); // realloc 을 같은 크기로 호출
-    *ptr2 = 20;
+    Test a;  // 생성자 호출
+    push_to_vector(vec, a);   // lvalue → 복사 발생
 
-    printf("ptr1 주소[%p]    ptr2 주소[%p] --- 같은 주소\n", ptr1, ptr2);
-    printf("ptr2 값[%d]\n", *ptr1);
-
-    printf("ptr2 주소[%p]", ptr2);
-    int* ptr3 = (int*)realloc(ptr2, sizeof(int) * 10); // realloc 을 보다 큰 크기로 호출
-    *ptr3 = 30;
-
-    printf("    ptr3 주소[%p] --- 달라진 주소\n", ptr3);
-    printf("ptr3 값[%d]\n", *ptr3);
-
-    return 0;
+    cout << endl;
+    push_to_vector(vec, Test()); // rvalue → 이동 발생
 }
