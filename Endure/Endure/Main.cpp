05c10e45081777c5771538b8a2579dc7d56ecb02
DESKTOP-K7E3CAS\chu03 <chu0326@naver.com>
2025-05-09 00:25:54 +0900

    스터디 추가본 및 수정

--- Endure/Endure/Main.cpp
+++ Endure/Endure/Main.cpp
@@ -1,59 +1,15 @@
-﻿#include <iostream>#include <bits/stdc++.h>
+﻿#include <iostream>
+#include <array>
 
 using namespace std;
 
-void insert(int idx, int num, int arr[], int& len) {
+int main()
+{
 
-	if (idx < 0)
-		return;
-	
-	int temp;
-	
-	for (int i = len; i > idx; --i)
-	{
+	const char name[] = "TEST";
 
-	}
-
-	arr[idx] = num;
-	
-	++len;
-}
+	auto& arr2 = name;  // 배열 참조 그대로 유지 const char(&)[8]
 
 
-void erase(int idx, int arr[], int& len) {
-
-}
-
-void printArr(int arr[], int& len) {
-	for (int i = 0; i < len; i++) cout << arr[i] << ' ';
-	cout << "\n\n";
-}
-
-void insert_test() {
-	cout << "***** insert_test *****\n";
-	int arr[10] = { 10, 20, 30 };
-	int len = 3; // 현재 배열의 길이 
-	insert(3, 40, arr, len); // 10 20 30 40
-	printArr(arr, len);
-	insert(1, 50, arr, len); // 10 50 20 30 40
-	printArr(arr, len);
-	insert(0, 15, arr, len); // 15 10 50 20 30 40
-	printArr(arr, len);
-}
-
-void erase_test() {
-	cout << "***** erase_test *****\n";
-	int arr[10] = { 10, 50, 40, 30, 70, 20 };
-	int len = 6;
-	erase(4, arr, len); // 10 50 40 30 20
-	printArr(arr, len);
-	erase(1, arr, len); // 10 40 30 20
-	printArr(arr, len);
-	erase(3, arr, len); // 10 40 30
-	printArr(arr, len);
-}
-
-int main(void) {
-	insert_test();
-	erase_test();
+	return 0;
 }
